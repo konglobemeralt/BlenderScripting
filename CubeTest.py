47706a6a0f13e181aee5baee3a834e08857f61a5
Konglobemeralt <jesper.blidkvist@live.se>
2017-06-14 22:40:24 +0200

    Creating cubes in a loop

--- CubeTest.py
+++ CubeTest.py
@@ -1,6 +1,11 @@
 import bpy #Imports the Blender Python API
 import mathutils #Imports Blender vector math utilities
 import math #Imports the standard Python math library
- 
-# Create a mesh cube in the scene
-bpy.ops.mesh.primitive_cube_add(location=(0.936607, -0.484878, 1.66298))
+
+for i in range(0, 5): 
+    x = i * 3
+    y = 3
+    z = 1
+
+    # Create a mesh cube in the scene
+    bpy.ops.mesh.primitive_cube_add(location=(x, y, z))
