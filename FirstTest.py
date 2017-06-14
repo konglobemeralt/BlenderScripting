05757f9d2ed0544754a530d0e9e0aaa2705fa7b6
Konglobemeralt <jesper.blidkvist@live.se>
2017-06-14 22:24:58 +0200

    Added first test script

---
+++ FirstTest.py
@@ -0,0 +1,15 @@
+import bpy #Imports the blender python API
+import mathutils #Imports blender vector math utils
+import math #Imports the standard Python math library
+
+#Set up some variables. Standard Python rules apply!
+a = 20
+b = 40
+
+#Print addition
+print(a + b)
+
+#Open console
+# Start blender from console to print stuff or use:
+# bpy.ops.wm.console_toggle() 
+# on windows
