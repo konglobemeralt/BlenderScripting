881a04dbdfeac0de574a066c95f7d63c8c528398
Konglobemeralt <jesper.blidkvist@live.se>
2017-06-14 22:43:37 +0200

    Cube grid

--- CubeTest.py
+++ CubeTest.py
@@ -2,10 +2,11 @@ import bpy #Imports the Blender Python API
 import mathutils #Imports Blender vector math utilities
 import math #Imports the standard Python math library
 
-for i in range(0, 5): 
-    x = i * 3
-    y = 3
-    z = 1
+for i in range(0, 10): 
+    for j in range (0, 10):
+        x = i * 3
+        y = j * 3
+        z = 1
 
-    # Create a mesh cube in the scene
-    bpy.ops.mesh.primitive_cube_add(location=(x, y, z))
+        # Create a mesh cube in the scene
+        bpy.ops.mesh.primitive_cube_add(location=(x, y, z))
