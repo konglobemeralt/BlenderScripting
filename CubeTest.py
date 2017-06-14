de2cfcd7a9d0fa1c92400389b1482317cbc9c964
Konglobemeralt <jesper.blidkvist@live.se>
2017-06-14 22:35:22 +0200

    Added basic script for primitive creation

---
+++ CubeTest.py
@@ -0,0 +1,6 @@
+import bpy #Imports the Blender Python API
+import mathutils #Imports Blender vector math utilities
+import math #Imports the standard Python math library
+ 
+# Create a mesh cube in the scene
+bpy.ops.mesh.primitive_cube_add(location=(0.936607, -0.484878, 1.66298))
